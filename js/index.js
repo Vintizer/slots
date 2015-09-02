b14c1827c469b3c5782a8e5e0171407cb7ba57ac
vintizer <vizerkharkiv@gmail.com>
2015-09-02 17:38:25 +0300

    Working not asyncronic

--- js/index.js
+++ js/index.js
@@ -1,50 +1,105 @@
+var settings = {
+  firstColumn: 10,
+  secondColumn: 26,
+  thirdColumn: 66,
+  picHeight: 100,
+  picWidth: 100,
+  firstPic: 'img/r1.gif',
+  secondPic: 'img/r1.gif',
+  thirdPic: 'img/r12.gif'
+};
+
 window.addEventListener('load', mainFunction);
+
 function mainFunction(){
-  //function
-  var funRect = [];
-  function rectAnimate(rect, count) {
-    var len = rect.length;
-    for (var i = 0; i < len; i++) {
-      rect[i].animate('top', '+=14', {
-        onChange: canvas.renderAll.bind(canvas)
-      })
-    }
+  var canvas1 = new fabric.Canvas('main1');
+  var canvas2 = new fabric.Canvas('main2');
+  var canvas3 = new fabric.Canvas('main3');
+  var group1;
+  var group2;
+  var group3;
+
+  function returnRandomFileName(min, max) {
+    var rand;
+    rand = Math.floor(Math.random() * (max - min + 1)) + min;
+    return 'img/r' + rand + '.gif';
   }
 
-  function sequence(fns, cb) {
-    var fnLength = fns.length+1;
-    done(0);
-    function done(j){
-      if (!--fnLength) cb();
-      else {
-        fns[j](function cb() {
-          done(j+1)
-        });
-      }
+  function settingImg(images, number, name, cb) {
+    var place;
+    var i;
+    var imgGroup = new fabric.Group();
+    for (i = 0; i < number + 2; i++) {
+      place = settings.picHeight * (1.5 - i);
+      images[i].set({ left: 0, top: place });
     }
+      fabric.Image.fromURL(name, function(img) {
+        images[number + 1] = img;
+        images[number + 1].set({ left: 0, top: settings.picHeight * (1.5 - number + 1) });
+        images.forEach(function(img) {
+          imgGroup.add(img);
+        });
+        cb(imgGroup);
+      })
   }
+
+  function generateGroupByCount(canvasNumber, cb) {
+    var images = [];
+    var i;
+    var name;
+    var number;
+    var placeInGroup;
+    switch (canvasNumber) {
+      case 1: number = settings.firstColumn;
+        placeInGroup = settings.firstPic;
+        break;
+      case 2: number = settings.secondColumn;
+        placeInGroup = settings.secondPic;
+        break;
+      case 3: number = settings.thirdColumn;
+        placeInGroup = settings.thirdPic;
+        break;
+    }
+    for (i = 0; i < number + 2; i++) {
+      name = returnRandomFileName(0,12);
+      console.log(name + ' ' + canvasNumber);
+      fabric.Image.fromURL(name, function(img) {
+        images.push(img);
+        if (images.length === number + 2) {
+          settingImg(images, number, placeInGroup, cb);
+        }
+    })}
+  }
+
+  generateGroupByCount(1, function(group) {
+    console.log('IT WORKS1' + group);
+    canvas1.add(group);
+    group1 = group;
+  });
+  generateGroupByCount(2, function(group) {
+    console.log('IT WORKS2' + group);
+    canvas2.add(group);
+    group2 = group;
+  });
+  generateGroupByCount(3, function(group) {
+    console.log('IT WORKS3' + group);
+    canvas3.add(group);
+    group3 = group;
+  });
   document.getElementById('btn').addEventListener('click', animateRect);
-  var canvas = new fabric.Canvas('main');
-  canvas.setWidth('350');
-  canvas.setHeight('350');
-  var rect = [];
-  rect[0] = new fabric.Rect({
-    left: 100,
-    top: -25,
-    fill: 'red',
-    width: 20,
-    height: 20
-  });
-  rect[1] = new fabric.Rect({
-    left: 100,
-    top: 0,
-    fill: 'red',
-    width: 20,
-    height: 20
-  });
-  canvas.add(rect[0]);
-  canvas.add(rect[1]);
+
   function animateRect(){
-    rectAnimate(rect, 4);
+    group1.animate('top', '+=' + (settings.firstColumn - 2) * settings.picHeight, {
+      duration: 5000,
+      onChange: canvas1.renderAll.bind(canvas1)
+          });
+    group2.animate('top', '+=' + (settings.secondColumn - 2) * settings.picHeight, {
+      duration: 5000,
+      onChange: canvas2.renderAll.bind(canvas2)
+    });
+    group3.animate('top', '+=' + (settings.thirdColumn - 2) * settings.picHeight, {
+      duration: 5000,
+      onChange: canvas3.renderAll.bind(canvas3)
+    });
   }
 }
